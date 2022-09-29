ae05d6298a64136d7944186e9089fbe7d4beb881
darts <darts>
2022-09-29 10:15:28 -0700

    4th Commit lab 2: at the end of part 4 of 6 fully works using viewBinding after the big picture of main.java

--- app/src/main/java/com/ui/MainActivity.java
+++ app/src/main/java/com/ui/MainActivity.java
@@ -1,6 +1,7 @@
-package com.example.lab2;
+package com.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.data.MainViewModel;
 import com.example.lab2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private MainViewModel model;
     private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
 //        setContentView(R.layout.activity_main);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView( variableBinding.getRoot());
@@ -22,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
         TextView mytext = variableBinding.textview;
         //with variable binding you would use: variableBinding.textview;
         // instead of TextView mytext = findViewById(R.id.textview);
-        Button mybutton = variableBinding.mybutton;
         EditText myedit = variableBinding.myedittext;
         String editString = myedit.getText().toString();
 
-        mybutton.setOnClickListener(vw -> mytext.setText("Your edit has: " + editString));
+        variableBinding.mybutton.setOnClickListener(click -> {
+            model.editString = variableBinding.myedittext.getText().toString();
+            mytext.setText("Your edit has: " + model.editString);//used mytext but could have used
+            //variableBinding.myText
+        });
     }
 }
